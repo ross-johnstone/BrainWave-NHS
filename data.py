23c56f0dc77edfb42c94f2899f4a83a93a7b07bb
Peter <2296505z@student.gla.ac.uk>
2019-11-20 10:35:42 +0000

    initial version of data input

---
+++ data.py
@@ -0,0 +1,18 @@
+# from scipy.io.wavfile import read as wavread
+# import numpy as np
+# import wave
+# waveform is 
+def read_wav(filename):
+	# frames=[]
+	with open(filename, 'rb') as infile:
+		data = infile.read(2)
+		print(data)
+	# rate, data = read(filename)
+	# print(data)
+	# data = wavread(filename)
+	# return np.array(data[1],dtype=float)
+
+def main ():
+	wav = read_wav("./data/recording1/pat1/08-12-2007_12_41_54_0000_000000.wav")
+if __name__ == "__main__":
+	main()

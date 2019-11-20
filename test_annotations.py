058bda65f44e3afe448301a7038170333e6ac97d
Xenia Skotti <46594038+XeniaSkotti@users.noreply.github.com>
2019-11-20 16:59:08 +0000

    cleanup test annotations

--- test_annotations.py
+++ test_annotations.py
@@ -7,15 +7,27 @@ from annotations import decode_annotation
 
 class TestAnnotations(unittest.TestCase):
 
+    def setUp(self):
+        annotations = []
+        annotations.append(Annotation("Test annotation 1", "This is the first test annotation.", 100, 250))
+        annotations.append(Annotation("Test annotation 2",
+                                      "This is the second test annotation, but it's a little bit longer just as an edge case.",
+                                      300, 350))
+        annotations.append(Annotation("Test annotation 3", "This is the third test annotation.", 455, 500))
+        annotations.append(Annotation("Test annotation 4",
+                                      "This is in fact not the fourth test annotation, just kidding it actually is.",
+                                      600, 700))
+        self.annotations = annotations
+
     def test_generate_annotations(self):
-        annotation = Annotation("Annotation 1", "This is the first test annotation.", 100, 250)
-        self.assertEqual(annotation.title, "Annotation 1", "Should be Annotation 1")
+        annotation = self.annotations[0]
+        self.assertEqual(annotation.title, "Test annotation 1", "Should be Annotation 1")
         self.assertEqual(annotation.content, "This is the first test annotation.", "Should be: This is the first test annotation." )
         self.assertEqual(annotation.start, 100, "Should be 100")
         self.assertEqual(annotation.end, 250, "Should be 250")
 
     def test_encode_decode_annotation(self):
-        annotation = Annotation("Annotation", "Encoding Annotation", 100, 250)
+        annotation = self.annotations[0]
         encoded_annotation = encode_annotation(annotation)
 
         self.assertEqual(isinstance(encoded_annotation, dict), True, "This should be a dictionary")
@@ -33,15 +45,7 @@ class TestAnnotations(unittest.TestCase):
 
 
     def test_save_open_annotations(self):
-        annotations = []
-        annotations.append(Annotation("Test annotation 1", "This is the first test annotation.", 100, 250))
-        annotations.append(Annotation("Test annotation 2",
-                                      "This is the second test annotation, but it's a little bit longer just as an edge case.",
-                                      300, 350))
-        annotations.append(Annotation("Test annotation 3", "This is the third test annotation.", 455, 500))
-        annotations.append(Annotation("Test annotation 4",
-                                      "This is in fact not the fourth test annotation, just kidding it actually is.",
-                                      600, 700))
+        annotations = self.annotations
         save_json(annotations)
         loaded_annotations = open_json("test.txt")
         self.assertEqual(len(loaded_annotations), len(annotations), "Should be: 4")

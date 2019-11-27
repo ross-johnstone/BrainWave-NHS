6299cfe571e775987a01671f6f2fd40020f5f6be
Pedro Castro <2397775d@student.gla.ac.uk>
2019-11-27 13:13:52 +0000

    Code now follows PEP-8 standard#

--- data.py
+++ data.py
@@ -5,76 +5,83 @@ import re
 import datetime
 from annotations import open_json
 
+
 def open_project(path):
-	"""
-		Given a directory path, open_project opens each .wav file as a numpy array,
-		stacks all of them into a single numpy array and returns it as the first element in result,
-		the timestamp as a datetime object as the second element in result 
-		and a list of Annotation objects  as the third element in result if a json file is present,
-		else it returns an empty list. 
-	"""
-	contents = os.listdir(path)
-	calfile = ""
-	datafiles = []
-	jsonfile = ""
-	# acquire filepaths for the different project files
-	for filepath in contents:
-		if re.match(r'\d{2}-\d{2}-\d{4}_\d{2}_\d{2}_\d{2}_\d{1,4}_\d*.cal', filepath):
-			calfile = path + filepath
-		elif re.match(r'\d{2}-\d{2}-\d{4}_\d{2}_\d{2}_\d{2}_\d{1,4}_\d*.wav', filepath):
-			datafiles.append(path + filepath)
-		elif re.match('.*\.json', filepath):
-			jsonfile = path + filepath
-	# sort datafiles to obtain the correct order of data
-	datafiles.sort()
-	raw_data = []
+    """
+        Given a directory path, open_project opens each .wav file as a numpy array,
+        stacks all of them into a single numpy array and returns it as the first element in result,
+        the timestamp as a datetime object as the second element in result
+        and a list of Annotation objects  as the third element in result if a json file is present,
+        else it returns an empty list.
+    """
+    contents = os.listdir(path)
+    calfile = ""
+    datafiles = []
+    jsonfile = ""
+    # acquire filepaths for the different project files
+    for filepath in contents:
+        if re.match(r'\d{2}-\d{2}-\d{4}_\d{2}_\d{2}_\d{2}_\d{1,4}_\d*.cal', filepath):
+            calfile = path + filepath
+        elif re.match(r'\d{2}-\d{2}-\d{4}_\d{2}_\d{2}_\d{2}_\d{1,4}_\d*.wav', filepath):
+            datafiles.append(path + filepath)
+        elif re.match('.*\.json', filepath):
+            jsonfile = path + filepath
+    # sort datafiles to obtain the correct order of data
+    datafiles.sort()
+    raw_data = []
 
-	# create a numpy array from the datafiles
-	for file in datafiles:
-		raw_data.append(read_wav(file))
-	data = np.hstack(raw_data)
+    # create a numpy array from the datafiles
+    for file in datafiles:
+        raw_data.append(read_wav(file))
+    data = np.hstack(raw_data)
 
-	# create timestamps 
-	timestamps = None
-	if calfile != "":
-		initial_time = get_initial_timestamp(calfile)
-		# timestamps = np.fromfunction(lambda i: np.datetime64(initial_time + datetime.timedelta(seconds = i/50)), data.shape)
-		timestamps = list(initial_time + datetime.timedelta(microseconds=1000*20*i) for i in range(data.shape[0]))
-	# load annotations
-	annotations = []
-	if jsonfile != "":
-		annotations = open_json(jsonfile)
+    # create timestamps
+    timestamps = None
+    if calfile != "":
+        initial_time = get_initial_timestamp(calfile)
+        # timestamps = np.fromfunction(lambda i: np.datetime64(initial_time + datetime.timedelta(seconds = i/50)), data.shape)
+        timestamps = list(initial_time + datetime.timedelta(microseconds=1000 * 20 * i) for i in range(data.shape[0]))
+    # load annotations
+    annotations = []
+    if jsonfile != "":
+        annotations = open_json(jsonfile)
 
-	return data, timestamps, annotations
+    return data, timestamps, annotations
+
 
 def read_wav(filename):
-	"""
-		Reads a single wav file in the format of two bytes is equal to one ICB reading and returns it as a numpy array
-	"""
-	frames=[]
-	with open(filename, 'rb') as infile:
-		raw_data = infile.read(2)
-		data = struct.unpack('h',raw_data)[0]
-		while raw_data != b"":
-			frames.append(data)
-			raw_data = infile.read(2)
-			if len(raw_data) == 2:
-				data = struct.unpack('h',raw_data)[0]
-	return np.array(frames)
-	
+    """
+        Reads a single wav file in the format of two bytes is equal to one ICB reading and returns it as a numpy array
+    """
+    frames = []
+    with open(filename, 'rb') as infile:
+        raw_data = infile.read(2)
+        data = struct.unpack('h', raw_data)[0]
+        while raw_data != b"":
+            frames.append(data)
+            raw_data = infile.read(2)
+            if len(raw_data) == 2:
+                data = struct.unpack('h', raw_data)[0]
+    return np.array(frames)
+
+
 def get_initial_timestamp(filename):
-	"""
-		Reads a .cal file and returns the initial timestamp
-	"""
-	timestamp = ""
-	with open(filename, 'r') as infile:
-		firstline = infile.readline()
-		match = re.match((r'(?P<timestamp>\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2})'), firstline)
-		timestamp = datetime.datetime.strptime(match.group(1), '%d-%m-%Y %H:%M:%S')
-		return timestamp
-def main ():
-	data, timestamps, annotations = open_project("./data/recording2/pat2/")
-	# do whatever with the data in testing
+    """
+        Reads a .cal file and returns the initial timestamp
+    """
+    timestamp = ""
+    with open(filename, 'r') as infile:
+        firstline = infile.readline()
+        match = re.match((r'(?P<timestamp>\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2})'), firstline)
+        timestamp = datetime.datetime.strptime(match.group(1), '%d-%m-%Y %H:%M:%S')
+        return timestamp
+
+
+def main():
+    data, timestamps, annotations = open_project("./data/recording2/pat2/")
+
+
+# do whatever with the data in testing
 
 if __name__ == "__main__":
-	main()
+    main()

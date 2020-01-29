02b7415f9582eff0ec4e3c9bbac081812b75d571
Xenia Skotti <46594038+XeniaSkotti@users.noreply.github.com>
2020-01-29 16:16:42 +0000

    added comment

--- delete_annotations.py
+++ delete_annotations.py
@@ -2,6 +2,7 @@ from annotations import Annotation, save_json
 
 annotation1 = Annotation("A1", "text", "start_time", "end_time")
 self.annotations = [annotation1]
+## get the annotation ID somhow from the interface and then search through the list, delete the annotation and save the new json file
 annotation_id = 1
 
 for i in range(len(self.annotations)):

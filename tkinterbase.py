73f1df620b8f3ff3ba97ade574d59d572899264c
Peter <2296505z@student.gla.ac.uk>
2020-02-12 11:51:26 +0000

    reformatted to PEP8

--- tkinterbase.py
+++ tkinterbase.py
@@ -15,6 +15,7 @@ from tkinter import messagebox
 
 
 class TkBase:
+
     def __init__(self, master, path):
 
         FIGSIZE = (8, 3)
@@ -23,31 +24,37 @@ class TkBase:
 
         master.title("BrainWave Visualization")
 
-        # create matplotlib figures with single axes on which the data will be displayed
+        # create matplotlib figures with single axes on which the data will be
+        # displayed
         self.main_graph, self.main_graph_ax = plt.subplots(figsize=FIGSIZE)
         self.main_graph.set_facecolor('xkcd:grey')
         self.main_graph_ax.set_facecolor('xkcd:dark grey')
 
         # second, reference graph
-        self.reference_graph, self.reference_graph_ax = plt.subplots(figsize=FIGSIZE)
+        self.reference_graph, self.reference_graph_ax = plt.subplots(
+            figsize=FIGSIZE)
         self.reference_graph.set_facecolor('xkcd:grey')
         self.main_graph_ax.set_facecolor('xkcd:dark grey')
         self.main_canvas = FigureCanvasTkAgg(self.main_graph, master=master)
-        self.main_canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
-        self.toolbar = NavigationToolbar(self.main_canvas, self.master, tkbase_=self)
+        self.main_canvas.get_tk_widget().pack(
+            side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
+        self.toolbar = NavigationToolbar(
+            self.main_canvas, self.master, tkbase_=self)
 
-        self.reference_canvas = FigureCanvasTkAgg(self.reference_graph, master=master)
-        self.reference_canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
-        self.reference_canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
-        
+        self.reference_canvas = FigureCanvasTkAgg(
+            self.reference_graph, master=master)
+        self.reference_canvas.get_tk_widget().pack(
+            side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
+        self.reference_canvas.get_tk_widget().pack(
+            side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
 
         self.project_path = path
         try:
-            self.data, self.timestamps, self.annotations = data.open_project(self.project_path)
+            self.data, self.timestamps, self.annotations = data.open_project(
+                self.project_path)
             self.draw_graph(self.data, self.timestamps, self.annotations)
         except Exception as e:
             messagebox.showerror("Error:", e)
-
 
         # put the plot with navbar on the tkinter window
         self.main_canvas.mpl_connect('button_release_event', self.butrelease)
@@ -69,12 +76,13 @@ class TkBase:
         self.span_min = None
         self.span_max = None
 
-    # callback method for the open button, opens an existing project        
+    # callback method for the open button, opens an existing project
     def open(self):
         path = filedialog.askdirectory()
         path = path + "/"
         try:
-            self.data, self.timestamps, self.annotations = data.open_project(path)
+            self.data, self.timestamps, self.annotations = data.open_project(
+                path)
             self.draw_graph(self.data, self.timestamps, self.annotations)
         except Exception as e:
             messagebox.showerror("Error:", e)
@@ -154,7 +162,8 @@ class TkBase:
             # method called when save button on popup is pressed
             def save():
                 if not title_entry.get().strip():
-                    error_label = Label(top, text="Please add a title!", fg="red")
+                    error_label = Label(
+                        top, text="Please add a title!", fg="red")
                     error_label.grid(row=3)
                 else:
                     new_annotation = Annotation(title_entry.get(), description_entry.get(1.0, tkinter.END),
@@ -165,7 +174,8 @@ class TkBase:
                     save_json(self.annotations, json_path)
                     self.draw_annotation(new_annotation)
 
-                    # set spans back to none after the annotation is saved to prevent buggy behavior
+                    # set spans back to none after the annotation is saved to
+                    # prevent buggy behavior
                     self.span_min = None
                     self.span_max = None
 
@@ -177,9 +187,12 @@ class TkBase:
             top.title('Confirm Annotation')
             top.grab_set()
 
-            # labels in top level window showing annotation start time and end time
-            annotation_start_label = Label(top, text='Annotation start time: ' + str(self.span_min))
-            annotation_end_label = Label(top, text='Annotation end time: ' + str(self.span_max))
+            # labels in top level window showing annotation start time and end
+            # time
+            annotation_start_label = Label(
+                top, text='Annotation start time: ' + str(self.span_min))
+            annotation_end_label = Label(
+                top, text='Annotation end time: ' + str(self.span_max))
             annotation_start_label.grid(row=0)
             annotation_end_label.grid(row=1)
 
@@ -193,13 +206,16 @@ class TkBase:
             description_entry = tkinter.Text(top, height=6, width=30)
             description_entry.grid(row=6)
 
-            save_button = Button(master=top, text="Save", command=save, bg='white')
+            save_button = Button(master=top, text="Save",
+                                 command=save, bg='white')
             save_button.grid(row=7)
 
-            cancel_button = Button(master=top, text="Cancel", command=cancel, bg='white')
+            cancel_button = Button(
+                master=top, text="Cancel", command=cancel, bg='white')
             cancel_button.grid(row=8)
 
-            # change button back to annotate button and hide span selector again
+            # change button back to annotate button and hide span selector
+            # again
             self.annotate_button.config(text='Annotate', command=self.annotate)
             self.span.set_visible(False)
 
@@ -214,13 +230,16 @@ class TkBase:
     # callback method of the span selector, after every selection it writes
     # the selected range to class variables
     def onselect(self, min, max):
-        self.span_min = datetime.datetime.fromordinal(int(min)) + datetime.timedelta(seconds=divmod(min, 1)[1] * 86400)
-        self.span_max = datetime.datetime.fromordinal(int(max)) + datetime.timedelta(seconds=divmod(max, 1)[1] * 86400)
+        self.span_min = datetime.datetime.fromordinal(
+            int(min)) + datetime.timedelta(seconds=divmod(min, 1)[1] * 86400)
+        self.span_max = datetime.datetime.fromordinal(
+            int(max)) + datetime.timedelta(seconds=divmod(max, 1)[1] * 86400)
 
     # get vertical range for a given annotation
     def get_vertical_range(self, annotation):
 
-        range_indices = np.where(np.logical_and(self.timestamps > annotation.start, self.timestamps < annotation.end))
+        range_indices = np.where(np.logical_and(
+            self.timestamps > annotation.start, self.timestamps < annotation.end))
 
         range_data = self.data[range_indices]
         return range_data[np.argmax(range_data)], range_data[np.argmin(range_data)]
@@ -229,11 +248,12 @@ class TkBase:
         # if date range annotation draw rectangle
         if (annotation.start != annotation.end):
             vmax, vmin = self.get_vertical_range(annotation)
-            tp = TextPath((date2num(annotation.start) + 6000, 300), annotation.title, size=100000)
+            tp = TextPath((date2num(annotation.start) + 6000, 300),
+                          annotation.title, size=100000)
             self.main_graph_ax.add_patch(PathPatch(tp, color="black"))
             self.main_graph_ax.add_patch(plt.Rectangle((date2num(annotation.start), vmin - 10),
-                                            date2num(annotation.end) - date2num(
-                                                annotation.start), vmax - vmin + 20, fc='r'))
+                                                       date2num(annotation.end) - date2num(
+                annotation.start), vmax - vmin + 20, fc='r'))
         # if point annotation draw a vertical line
         if (annotation.start == annotation.end):
             plt.figure(1)
@@ -243,28 +263,29 @@ class TkBase:
 
     def draw_graph(self, data, timestamps, annotations):
         self.main_graph_ax.clear()
-        #plot values on the axe and set plot hue to NHS blue
+        # plot values on the axe and set plot hue to NHS blue
         self.main_graph_ax.plot(timestamps, data, color='#5436ff')
-        #draw all saved annotations
+        # draw all saved annotations
         for annotation in annotations:
             self.draw_annotation(annotation)
 
         self.main_graph_ax.xaxis_date()
         plt.gcf().autofmt_xdate()
-        #adding grid
-        self.main_graph_ax.grid(color='grey',linestyle='-', linewidth=0.25, alpha=0.5)
-        #removing top and right borders
+        # adding grid
+        self.main_graph_ax.grid(
+            color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
+        # removing top and right borders
         self.main_graph_ax.spines['top'].set_visible(False)
         self.main_graph_ax.spines['right'].set_visible(False)
         # put the plot with navbar on the tkinter window
         self.main_canvas.draw()
 
-
         self.toolbar.update()
 
         # second, reference graph displayed
         self.reference_graph_ax.clear()
-        self.reference_graph_ax.plot(self.timestamps, self.data, color="cyan", linewidth=1)
+        self.reference_graph_ax.plot(
+            self.timestamps, self.data, color="cyan", linewidth=1)
         self.reference_graph_ax.xaxis_date()
         # put the second plot on the tkinter window
         self.reference_canvas.draw()
@@ -293,7 +314,8 @@ class NavigationToolbar(NavigationToolbar2Tk):
             ('Open', 'Opens a new project', 'open', 'call_open'),
             ('Export', 'Export to PDF', 'export', 'call_export'),
             ('Save', 'Save the figure', 'filesave', 'save_figure'),
-            ('Open Concurrent', 'Open a concurrent graph view', 'compare', 'call_open_concurrent'),
+            ('Open Concurrent', 'Open a concurrent graph view',
+             'compare', 'call_open_concurrent'),
             (None, None, None, None),
             ('Quit', 'Quit application', 'quit', 'call_quit'),
         )
